1874e6b121e3c03f3e4c5d2080904e0269694026
Angel Velasco <angelgg2020@outlook.com>
2023-09-16 11:05:46 -0500

    feat: :wrench: Express server run succesfully usign config files

---
+++ api/config/credentials.js
@@ -0,0 +1,10 @@
+import dotenv from "dotenv"
+dotenv.config();
+
+export const CONFIG = {
+    "server":  JSON.parse(process.env.SERVER),
+    "user": process.env.ATLAS_USER,
+    "password": process.env.ATLAS_PASSWORD,
+    "db": process.env.ATLAS_DB,
+    "private_key": process.env.PRIVATE_KEY
+}
