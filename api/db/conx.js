cf6df86eddeeae53dabd6bd584288c28afb9b1e5
Angel Velasco <angelgg2020@outlook.com>
2023-09-16 12:16:41 -0500

    feat: :card_file_box: Database connection succesfully.
    Insert some data, and implementing autoIncrement.

---
+++ api/db/conx.js
@@ -0,0 +1,17 @@
+import { MongoClient } from "mongodb";
+import { CONFIG } from "../config/credentials.js"
+
+export const getConx = async () => {
+    try {
+        const uri = `mongodb+srv://${CONFIG.user}:${CONFIG.password}@cluster0.tfk8jyc.mongodb.net/${CONFIG.db}`;
+        const options = {
+            retryWrites: true
+        }
+
+        const client = await MongoClient.connect(uri, options);
+        return client.db();
+    } 
+    catch (err) {
+        return { status: 500, message: err.message }
+    }
+}
---
+++ api/helpers/autoincrement.js
@@ -0,0 +1,13 @@
+import { getConx } from "../db/conx";
+
+const db = await getConx();
+
+export const autoIncrement = (collection) => {
+    const counters = db.collection("counters");
+    const response = db.counters.findOneAndUpdate(
+       { counter: `${collection}Id`},
+       { $inc: { sequence: 1 }},
+       { returnDocument: "after" }
+    )
+    return response.value.sequence;
+}
