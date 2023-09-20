5c6f1515caf3a1426f256306b62117bd58e44301
Angel Velasco <angelgg2020@outlook.com>
2023-09-20 18:18:50 -0500

    feat: :lipstick: Strat with login component

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './styles/index.css'
+import ReactDOM from 'react-dom/client';
+import { Router } from "./App";
+import '@mantine/core/styles.css';
+import './styles/index.css';
+import { MantineProvider } from '@mantine/core';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <MantineProvider>
+    <Router />
+  </MantineProvider>,
 )
---
+++ src/views/Login.jsx
@@ -0,0 +1,7 @@
+import { Login } from "../components/Login";
+
+export const LoginView = () => {
+    return (
+        <Login />
+    )
+}
