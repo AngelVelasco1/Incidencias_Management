7bd10bc54b72d2ce5f84aac1a79d2d100db0ec7b
Angel Velasco <angelgg2020@outlook.com>
2023-09-21 14:44:15 -0500

    feat: :pencil2: Change server host and port

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 5082,
+    host: "127.1.1.2"
+  }
 })
