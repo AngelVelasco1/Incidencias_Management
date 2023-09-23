9cc6cc918ff4df9eacabc3be05a40c2be7209065
Angel Velasco <angelgg2020@outlook.com>
2023-09-23 12:19:33 -0500

    feat: :lipstick: Basic login style

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -28,24 +28,24 @@ export const Login = () => {
 
   return (
     <>
-      <form >
-        <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+      <form className="font-mono">
+        <div className="flex min-h-full flex-1 flex-col justify-center bg-slate-900	rounded-2xl	 px-6 py-32 lg:px-48 ">
           <div className="sm:mx-auto sm:w-full sm:max-w-sm">
             <img
               className="mx-auto h-10 w-auto"
               src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
               alt="Your Company"
             />
-            <h1 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+            <h1 className="mt-10 text-center text-3xl font-bold leading-9 tracking-tight text-white-900">
               Welcome to Incidences
             </h1>
-            <h2>Login to your account</h2>
+            <h2 className="text-center text-xl tracking-tight text-white-800 mb-8">Login to your account</h2>
           </div>
-        </div>
-        <Button color="default" variant="ghost" onClick={handleDiscordLogin}>
-          <img id="discordImg" src="./src/assets/discord.png "></img>
+        <Button className="justify-evenly bg-gradient-to-tr from-zinc-100 to-zinc-300 text-indigo-500 text-xl font-bold" size="lg"  variant="light" onClick={handleDiscordLogin}>
+          <img  width="42" src="./src/assets/discord.png "></img>
           Discord
         </Button>
+        </div>
       </form>
 
     </>
--- src/main.jsx
+++ src/main.jsx
@@ -8,10 +8,7 @@ import './styles.css'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
   <NextUIProvider>
-    <main className="dark text-foreground bg-background">
-
     <Router />
-    </main>
   </NextUIProvider>
 </React.StrictMode>,
 )
