020de64b52eee2b6cb77b8a9e39a683b578fd824
Angel Velasco <angelgg2020@outlook.com>
2023-10-02 09:24:06 -0500

    feat: :lipstick: Start ui incidences cards

--- api/routes/login.js
+++ api/routes/login.js
@@ -4,7 +4,7 @@ import { CONFIG } from "../config/credentials.js";
 
 const loginStorage = Router();
 
-loginStorage.get("/auth", passport.authenticate('discord'));
+loginStorage.get("/auth", passport.authenticate('discord') );
 loginStorage.get("/auth/redirect", passport.authenticate('discord', {
     successRedirect: `http://${CONFIG.hostname}:${CONFIG.frontend_port}/dashboardCamper`,
     failureRedirect: `http://${CONFIG.hostname}:${CONFIG.frontend_port}/login`,
