9992bc28a5cf3c54822fa7ba4c5cff89bfe4f85d
Angel Velasco <angelgg2020@outlook.com>
2023-10-04 06:40:26 -0500

    style: :lipstick: Improve Not found page style and funcionality}

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, Button } from "@nextui-org/react";
 import { Image } from '@nextui-org/react'
 
     
 export const NotFound = () => {
   return (
-    <Image src='./src/assets/404_error.png' width={700} height={100}></Image>
+    <>
+    <div className= "flex  items-center flex-col w-screen h-screen bg-[#EEE]">
+    <Image className='mt-20' src='./src/assets/404_error.png' width={800} height={100}></Image>
+     <Button
+     className= " w-1/8"
+      href="/"
+      as={Link}
+      color="primary"
+      variant="flat"
+    >
+      Go back
+    </Button>
+    </div>
+
+    </>
+   
   )
 }
